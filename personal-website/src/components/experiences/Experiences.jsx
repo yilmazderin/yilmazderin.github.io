b3a2c435b1006205decdbdd0682884b39fb447b7
yilmazderin <dyilmaz2@uwo.ca>
2023-01-07 14:17:40 -0500

    Updating experiences

--- personal-website/src/components/experiences/Experiences.jsx
+++ personal-website/src/components/experiences/Experiences.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import "./experiences.css";
 import etds from '../../assets/etds-modified.png'
+import ws from '../../assets/wslogo.png'
+import wai from '../../assets/wai.png'
 
 const Services = () => {
     return (
         <section className="experiences section container" id='experiences'>
             <h2 className="section__title">Experiences</h2>
 
-            <div className="experiences__container grid">
+            <div className="experience1__container grid">
+
+                <img src={ws} alt="" className="ws" />
+                <div className="experience1__data grid">
+                    <div className="experience1__info">
+                        <h2 className="experience1__title">Wired Solutions</h2>
+                        <h3 className="experience1__name">Junior Developer</h3>
+                        <p className="experience1__description">I created ...</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="experience1__container grid">
 
                 <img src={etds} alt="" className="etds" />
+                <div className="experience1__data grid">
+                    <div className="experience1__info">
+                        <h2 className="experience1__title">Eagle Truck Driving School</h2>
+                        <h3 className="experience1__name">Web Developer</h3>
+                        <p className="experience1__description">I created ...</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="experience1__container grid">
+
+                <img src={wai} alt="" className="wai" />
+                <div className="experience1__data grid">
+                    <div className="experience1__info">
+                        <h2 className="experience1__title">Western AI</h2>
+                        <h3 className="experience1__name">Web Developer</h3>
+                        <p className="experience1__description">I created ...</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
