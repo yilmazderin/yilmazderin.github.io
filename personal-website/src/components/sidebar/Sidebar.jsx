a34fdb4bb151f1d5d1dd941d9c751835fd59cc89
yilmazderin <dyilmaz2@uwo.ca>
2023-01-07 00:11:08 -0500

    Sidebar complete

--- personal-website/src/components/sidebar/Sidebar.jsx
+++ personal-website/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
 import "./sidebar.css";
+import Logo from '../../assets/dy-logo-removebg-preview.png';
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <aside className='aside'>
+            <a href="#home" className="nav__logo">
+                <img src = {Logo} className= "logo_pic" alt = '' />
+            </a>
+
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="navlist">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                                <i className='icon-home'></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                            <i className='icon-user-following'></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#services" className="nav__link">
+                            <i className='icon-briefcase'></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#resume" className="nav__link">
+                            <i className='icon-graduation'></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                            <i className='icon-layers'></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#blog" className="nav__link">
+                            <i className='icon-note'></i>
+                            </a>
+                        </li>
+                        
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                            <i className='icon-bubble'></i>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+
+            <div className="nav__footer">
+                <span className="copyright">&copy; 2022-2023. </span>
+            </div>
+        </aside>
     )
 }
 
