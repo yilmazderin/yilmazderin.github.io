9c8d430d50201b2c13913a6fa43abcedc6c1c2db
yilmazderin <dyilmaz2@uwo.ca>
2023-01-07 02:00:48 -0500

    Home finished

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import "./home.css";
+import ME from '../../assets/derin.png'
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="home container" id="home">
+            <div className="intro">
+                <img src = {ME} alt = "" className="personal-pic" />
+                <h1 className="home__name">Derin Yilmaz</h1>
+                <span className="home__education">I am a 3rd year Software Engineering student</span>
+
+                <HeaderSocials />
+
+                <a href='#contact' className='btn'>Get to know me!</a>
+
+                <ScrollDown />
+            </div>
+        </section>
     )
 }
 
