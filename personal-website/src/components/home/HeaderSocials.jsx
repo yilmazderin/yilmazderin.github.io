1595dfeaac0087503a5c1f9b07d9b25bd30a04ec
yilmazderin <dyilmaz2@uwo.ca>
2023-01-09 11:52:55 -0500

    Updated links

--- personal-website/src/components/home/HeaderSocials.jsx
+++ personal-website/src/components/home/HeaderSocials.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const HeaderSocials = () => {
     return (
         <div className="home__socials">
-            <a href="" className="home__social-link" target="_blank">
+            <a href="https://www.facebook.com/derin.yilmaz.5623293/" className="home__social-link" target="_blank">
             <i class="fa-brands fa-facebook"></i>
             </a>
-            <a href="" className="home__social-link" target="_blank">
+            <a href="https://www.linkedin.com/in/derinyilmaz/" className="home__social-link" target="_blank">
             <i class="fa-brands fa-linkedin"></i>
             </a>
-            <a href="" className="home__social-link" target="_blank">
+            <a href="https://github.com/yilmazderin" className="home__social-link" target="_blank">
             <i class="fa-brands fa-github"></i>
             </a>
         </div>
