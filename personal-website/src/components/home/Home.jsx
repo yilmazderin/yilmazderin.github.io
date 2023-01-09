1932f80e1bce9f641dffd230bdf97ef6a98fa0c8
yilmazderin <dyilmaz2@uwo.ca>
2023-01-09 18:01:12 -0500

    Made shapes for home page

--- personal-website/src/components/home/Home.jsx
+++ personal-website/src/components/home/Home.jsx
@@ -3,23 +3,27 @@ import "./home.css";
 import ME from '../../assets/derin.png'
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
+import Shapes from './Shapes';
 
 const Home = () => {
     return (
         <section className="home container" id="home">
+            
             <div className="intro">
                 <img src = {ME} alt = "" className="personal-pic" />
                 <h1 className="home__name">Derin Yilmaz</h1>
                 <span className="home__education">I am a 3rd year Software Engineering student</span>
 
                 <HeaderSocials />
+                
 
                 <a href='#contact' className='btn'>Get to know me!</a>
 
                 <ScrollDown />
             </div>
+            <Shapes />
         </section>
-    )
-}
+    );
+};
 
 export default Home
