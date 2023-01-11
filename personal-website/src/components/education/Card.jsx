93d52baf1de75fed928a4d9c121da570478624cb
Derin Yilmaz <dyilmaz2@uwo.ca>
2023-01-11 01:19:13 -0500

    Updated experiences

--- personal-website/src/components/education/Card.jsx
+++ personal-website/src/components/education/Card.jsx
@@ -7,6 +7,8 @@ const Card = (props) => {
             <span className="timeline__date">{props.year}</span>
             <h3 className="timeline__title">{props.title}</h3>
             <div className="timeline__text">{props.desc}</div>
+            <div className="timeline_text">{props.desc1}</div>
+            <div className="timeline_text">{props.desc2}</div>
         </div>
     )
 }
